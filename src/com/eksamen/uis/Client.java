fa24fa7978c32f6bff0ac049229be52f13dfeb9d
dyb0 <dybevik2@gmail.com>
2021-04-28 19:46:30 +0200

    client v3

--- src/com/eksamen/uis/Client.java
+++ src/com/eksamen/uis/Client.java
@@ -1,20 +1,25 @@
 package com.eksamen.uis;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Client {
     private BorderPane clientGui;
     private HBox clientGuiBottom;
+    private VBox clientGuiCenter;
     private Button sendBtn;
     private TextField clientGuess;
     private TextField serverDescription;
+    private Label lbl;
 
     public Client() {
         makeClientGui();
@@ -23,14 +28,20 @@ public class Client {
     public void makeClientGui(){
         clientGui = new BorderPane();
         clientGuiBottom = new HBox();
+        clientGuiCenter = new VBox();
+        lbl = new Label("Gjett ordet her");
         clientGui.setPrefSize(500, 500);
         sendBtn = new Button("Gjett");
+        clientGuess = new TextField();
         serverDescription = new TextField();
         serverDescription.setEditable(false);
-        clientGuess = new TextField("Gjett hva ordet er her");
-        clientGui.setCenter(clientGuess);
+        serverDescription.setPrefHeight(200);
+        clientGui.setCenter(clientGuiCenter);
         clientGui.setBottom(clientGuiBottom);
         clientGui.setTop(serverDescription);
+        clientGuiCenter.setMargin(lbl, new Insets(50,0,0,0));
+        clientGuiCenter.setSpacing(20);
+        clientGuiCenter.getChildren().addAll(lbl, clientGuess);
         clientGuiBottom.getChildren().add(sendBtn);
         clientGuiBottom.setAlignment(Pos.CENTER);
     }
