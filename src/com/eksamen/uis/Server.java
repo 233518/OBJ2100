12f2c2fa08a15754d9eaa107980a86e5dc259a88
Govert <govert@live.no>
2021-04-28 19:01:23 +0200

    lololololololol grids

--- src/com/eksamen/uis/Server.java
+++ src/com/eksamen/uis/Server.java
@@ -1,5 +1,30 @@
 package com.eksamen.uis;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
 public class Server {
+
+    public void spillVindu{
+
+    // Komponenter i bruk
+    Text question1 = new Text("Hva er ordet?");
+    TextField ord = new TextField();
+    Text question2 = new Text("Forklaring for ordet?");
+    TextField beskrivelse = new TextField();
+    Button nesteKnapp = new Button("Neste");
+
+    // Oppsett
+    GridPane spill = new GridPane();
+    spill.setPadding(new Insets(10,10,10,10));
+    spill.add(question1, 0, 0);
+    spill.add(ord, 0, 1);
+    spill.add(question2, 0, 2);
+    spill.add(beskrivelse, 0, 3);
+    spill.add(nesteKnapp, 0, 4);
+    }
 
 }
