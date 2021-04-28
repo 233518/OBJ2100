6c39e0eed8da9f1f6911e3bdf157901d42638349
233511 <sigve888@gmail.com>
2021-04-28 18:00:25 +0200

    Player og Score

--- src/com/eksamen/components/Player/Player.java
+++ src/com/eksamen/components/Player/Player.java
@@ -1,6 +1,18 @@
 package com.eksamen.components.Player;
 
+/**
+ * Klasse til spillere.
+ * @author Sigve
+ */
 public class Player {
-    private Score score;
+    private Score score; //Poeng
+    private boolean tur; //Er det denne spilleren sin tur?
+
+
+
+
+
+
+
 
 }
--- src/com/eksamen/components/Player/Score.java
+++ src/com/eksamen/components/Player/Score.java
@@ -1,16 +1,31 @@
 package com.eksamen.components.Player;
 
+/**
+ * Klasse som tar vare på poeng som spilleren har tjent.
+ * @author Sigve
+ */
 public class Score {
     private int score;
 
+    /**
+     * Constructor for poeng
+     */
     public Score(){
         this.score = 0;
     }
 
+    /**
+     * Metode for å legge til poeng til den totale poengsummen
+     * @param points Antall poeng du får
+     */
     public void scorePoints(int points) {
         this.score = this.score + points;
     }
 
+    /**
+     * Metode for å returnere den totale poengsummen
+     * @return Terurnerer total poengsum.
+     */
     protected int getScore() {
         return score;
     }
