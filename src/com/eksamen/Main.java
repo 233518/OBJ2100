141013f17a8525f3d79bf3f3bc488205e331faa8
233518 <233518@usn.no>
2021-04-28 16:54:33 +0200

    Sjekk om sqlite funker

--- src/com/eksamen/Main.java
+++ src/com/eksamen/Main.java
@@ -1,19 +1,29 @@
 package com.eksamen;
 
+import com.eksamen.networking.Server;
+import com.eksamen.uis.MainUi;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main extends Application {
+    public Scene scene;
+    public static Server server;
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+    public void start(Stage stage) throws SQLException {
+
+        server = new Server();
+        server.start();
+
+        Connection con = DriverManager.getConnection("jdbc:sqlite:OppgaveDB.db");
+        scene = new Scene(new MainUi().getGrid(), 640, 480);
+
         stage.setScene(scene);
         stage.show();
     }
