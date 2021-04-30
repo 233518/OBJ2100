377e520b4a01f438ca0c2a170c9867062ef49433
233518 <233518@usn.no>
2021-04-30 19:15:28 +0200

    Gjort klart til eksamen

--- EksamensTest/Main.java
+++ EksamensTest/Main.java
@@ -1,13 +1,20 @@
 package com.eksamen;
 
+import com.eksamen.scenes.ServerScene;
+import com.eksamen.uis.ServerUi;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.sql.SQLException;
 
 public class Main extends Application {
+    public static ServerScene scene;
+
     @Override
     public void start(Stage stage) throws SQLException {
+        scene = new ServerScene(new ServerUi());
+        stage.setScene(scene);
+
         stage.show();
     }
     public static void main(String[] args) {
--- EksamensTest/MainClient.java
+++ EksamensTest/MainClient.java
@@ -1,10 +1,20 @@
 package com.eksamen;
+
+import com.eksamen.networking.Server;
+import com.eksamen.scenes.ClientScene;
+import com.eksamen.uis.ClientUi;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class MainClient extends Application {
+    public ClientScene scene;
+    public static Server server;
+
     @Override
     public void start(Stage stage) {
+        scene = new ClientScene(new ClientUi());
+
+        stage.setScene(scene);
         stage.show();
     }
     public static void main(String[] args) {
