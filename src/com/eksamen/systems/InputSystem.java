0532bbdd339a2a27e3c560bb37f206c012eac477
dyb0 <dybevik2@gmail.com>
2021-05-03 16:32:38 +0200

    meldingsystem koblet til knapp

--- src/com/eksamen/systems/InputSystem.java
+++ src/com/eksamen/systems/InputSystem.java
@@ -1,15 +1,22 @@
 package com.eksamen.systems;
 
+import com.eksamen.components.Bruker;
 import com.eksamen.uis.layouts.RomChat;
 import javafx.scene.control.Button;
 
 public class InputSystem {
     private RomChat romChat;
+    private MessageSystem message;
+    private Bruker bruker;
+
+    public InputSystem(){
+
+    }
 
     public void sendMelding(){
         romChat.getSendKnapp().setOnAction(actionEvent -> {
             String melding = romChat.getMeldingsBoks().getText();
-            romChat.getMeldingsBoks().setText("");
+            message = new MessageSystem(bruker.getName(), melding);
         });
     }
 
