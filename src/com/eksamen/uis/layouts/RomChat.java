11098c71695348f34d7239dfff8fa1f8bffe0a13
dyb0 <dybevik2@gmail.com>
2021-05-03 14:18:50 +0200

    addet deltakerliste

--- src/com/eksamen/uis/layouts/RomChat.java
+++ src/com/eksamen/uis/layouts/RomChat.java
@@ -2,10 +2,7 @@ package com.eksamen.uis.layouts;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -13,10 +10,14 @@ import javafx.scene.layout.VBox;
 public class RomChat {
     private TextField meldingsBoks;
     private TableView inndata;
+    private TableView deltakere;
+    private Label deltakereLabel;
+    private Label inndataLabel;
     private Button sendKnapp;
     private BorderPane borderpane;
     private HBox bottomBorderPane;
     private VBox centerBorderPane;
+    private VBox rightBorderPane;
 
     public RomChat(){
         lagRomChat();
@@ -27,20 +28,25 @@ public class RomChat {
         borderpane = new BorderPane();
         bottomBorderPane = new HBox();
         centerBorderPane = new VBox();
+        rightBorderPane = new VBox();
         inndata = new TableView();
+        deltakere = new TableView();
         sendKnapp = new Button("Send");
         meldingsBoks = new TextField();
+        deltakereLabel = new Label("Deltakere");
+        inndataLabel = new Label("Meldinger");
 
         //Plasserer panes
         borderpane.setBottom(bottomBorderPane);
         borderpane.setCenter(centerBorderPane);
+        borderpane.setRight(rightBorderPane);
 
         //Setter størrelser
         borderpane.setMinSize(400, 500);
         bottomBorderPane.setMinSize(400,50);
+        deltakere.setMinSize(100,450);
         inndata.setMinSize(350,450);
-        inndata.setMaxSize(380,450);
-        meldingsBoks.setMinSize(300, 35);
+        meldingsBoks.setMinSize(500, 35);
         sendKnapp.setPrefSize(50,35);
 
         //Posisjonerer
@@ -48,15 +54,17 @@ public class RomChat {
 
         //Modifikasjon til inndata
         inndata.setEditable(false);
+        deltakere.setEditable(false);
 
         //Spacing mellom komponenter
         bottomBorderPane.setSpacing(20);
         bottomBorderPane.setMargin(meldingsBoks, new Insets(0, 0, 0, 20));
-        centerBorderPane.setMargin(inndata, new Insets(0, 0,20,0));
+        centerBorderPane.setMargin(inndata, new Insets(0, 20,20,0));
 
         //Legger til komponenter i panes
-        centerBorderPane.getChildren().add(inndata);
+        centerBorderPane.getChildren().addAll(inndataLabel, inndata);
         bottomBorderPane.getChildren().addAll(meldingsBoks, sendKnapp);
+        rightBorderPane.getChildren().addAll(deltakereLabel, deltakere);
     }
 
     public BorderPane getBorderpane() {
