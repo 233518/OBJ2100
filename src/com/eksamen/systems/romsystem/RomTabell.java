acdc7add5bc069663be1209a0979d9a22113eaa4
233511 <sigve888@gmail.com>
2021-05-03 22:45:51 +0200

    La til JavaDoc og fjernet ubrukte metoder.

--- src/com/eksamen/systems/romsystem/RomTabell.java
+++ src/com/eksamen/systems/romsystem/RomTabell.java
@@ -1,38 +1,30 @@
 package com.eksamen.systems.romsystem;
 
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * Klasse for å opprette rad i TableView
+ */
 public class RomTabell {
     private SimpleStringProperty romNavn;
     private SimpleStringProperty opprettetNavn;
 
+    /**
+     * Constructor for å lage rad i TableView
+     * @param romNavn Navnet på rommet.
+     * @param opprettetNavn Navnet til brukeren som opprettet rommet.
+     */
     public RomTabell(String romNavn, String opprettetNavn) {
         this.romNavn = new SimpleStringProperty(romNavn);
         this.opprettetNavn = new SimpleStringProperty((opprettetNavn));
     }
 
+    /**
+     * Metode for å returnere romnavnet.
+     * @return Returnerer romnavnet.
+     */
     public String getRomNavn() {
         return romNavn.get();
     }
 
-    public SimpleStringProperty romNavnProperty() {
-        return romNavn;
-    }
-
-    public String getOpprettetNavn() {
-        return opprettetNavn.get();
-    }
-
-    public SimpleStringProperty opprettetNavnProperty() {
-        return opprettetNavn;
-    }
-
-    public void setRomNavn(SimpleStringProperty romNavn) {
-        this.romNavn = romNavn;
-    }
-
-    public void setOpprettetNavn(SimpleStringProperty opprettetNavn) {
-        this.opprettetNavn = opprettetNavn;
-    }
 }
