2043c76944764a6a6578faf8ba9156dfa3c31d1b
dyb0 <dybevik2@gmail.com>
2021-05-03 13:42:49 +0200

    begynt romsystem

--- src/com/eksamen/systems/RomSystem.java
+++ src/com/eksamen/systems/RomSystem.java
@@ -1,4 +1,25 @@
 package com.eksamen.systems;
 
+import com.eksamen.components.Bruker;
+import com.eksamen.components.Rom;
+import com.eksamen.uis.layouts.RomListe.RomListe;
+import com.eksamen.uis.layouts.RomListe.RomTabell;
+
+
 public class RomSystem {
+    private Rom rom;
+    private Bruker bruker;
+    private RomListe romliste;
+
+    public RomSystem(Rom rom){
+        this.rom = rom;
+    }
+
+    public void lagRom(){
+        //rom = new Rom(1, "Hey", bruker.getName());
+    }
+
+    public void bliMedIRom(){
+        RomTabell rom = romliste.getRomTableView().getSelectionModel().getSelectedItem();
+    }
 }
