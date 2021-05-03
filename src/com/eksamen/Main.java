c3ce86cf98b41a4e2c7314a145aab9957123b41a
233518 <233518@usn.no>
2021-05-03 13:44:34 +0200

    Testing

--- src/com/eksamen/Main.java
+++ src/com/eksamen/Main.java
@@ -6,20 +6,30 @@ import com.eksamen.scenes.ServerScene;
 import com.eksamen.scenes.TestSceneServer;
 import com.eksamen.uis.ClientUi;
 import com.eksamen.uis.ServerUi;
+import com.eksamen.uis.layouts.TestLayout;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.sql.SQLException;
 
 public class Main extends Application {
+    public static boolean debug = false;
     private ServerScene scene;
     private ServerUi serverUi;
+    private TestLayout testLayout;
 
     @Override
     public void start(Stage stage) {
-        serverUi = new ServerUi();
-        scene = new ServerScene(stage, serverUi);
-        stage.setScene(scene);
+        if(debug) {
+            serverUi = new ServerUi();
+            testLayout = new TestLayout();
+            TestSceneServer sceneTest = new TestSceneServer(stage, testLayout);
+            stage.setScene(sceneTest);
+        } else {
+            serverUi = new ServerUi();
+            scene = new ServerScene(stage, serverUi);
+            stage.setScene(scene);
+        }
         stage.show();
     }
     public static void main(String[] args) {
--- src/com/eksamen/scenes/TestSceneClient.java
+++ src/com/eksamen/scenes/TestSceneClient.java
@@ -4,30 +4,20 @@ import com.eksamen.networking.ClientNetworking;
 import com.eksamen.systems.MessageSystem;
 import com.eksamen.uis.ClientUi;
 import com.eksamen.uis.ServerUi;
+import com.eksamen.uis.layouts.TestLayout;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class TestSceneClient extends Scene {
     private ClientNetworking nettverk;
-    private ClientUi clientUi;
-    private MessageSystem messageSystem;
-    public TestSceneClient(ClientUi clientUi) {
-        super(clientUi.getHovedPane());
-        this.clientUi = clientUi;
+    private TestLayout testLayout;
+    public TestSceneClient(Stage stage, TestLayout testLayout) {
+        super(testLayout.getPane());
+        this.testLayout = testLayout;
         nettverk = new ClientNetworking();
         nettverk.start();
-
-        messageSystem = new MessageSystem();
-
-
-
-        clientUi.getTest().getButton1().setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                nettverk.sendMessage();
-            }
-        });
     }
 }
