7e67c74ecdf745dc766196a9e71633523cfbc643
233511 <sigve888@gmail.com>
2021-05-03 22:01:45 +0200

    Javadoc og ryddet opp i kode.

--- src/com/eksamen/systems/romsystem/RomTableView.java
+++ src/com/eksamen/systems/romsystem/RomTableView.java
@@ -1,17 +1,13 @@
 package com.eksamen.systems.romsystem;
 
-import com.eksamen.components.Bruker;
-import com.eksamen.components.Rom;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.util.ArrayList;
 
 public class RomTableView {
-    //private ArrayList<Rom> romArrayList = new ArrayList<Rom>();
     private TableView<RomTabell> romTableView = new TableView<>();
 
     private TableView tableView() {
@@ -19,15 +15,11 @@ public class RomTableView {
             TableColumn<RomTabell, String> romKolonne = new TableColumn<>("Rom");
             TableColumn<RomTabell, String> brukerKolonne = new TableColumn<>("Laget av");
 
-
             romKolonne.setMinWidth(50);
             romKolonne.setCellValueFactory(new PropertyValueFactory<RomTabell, String>("romNavn"));
 
             brukerKolonne.setMinWidth(50);
             brukerKolonne.setCellValueFactory(new PropertyValueFactory<RomTabell, String>("opprettetNavn"));
-
-            //Midlertidig til testing
-            //RomSystem.opprettRomOgBruker();
 
             romTableView.getColumns().addAll(romKolonne, brukerKolonne);
         }catch (Exception e){
@@ -36,6 +28,11 @@ public class RomTableView {
         return romTableView;
     }
 
+    /**
+     * Metode for å oppdatere TableViewen med rom som blir lagt til ved å lese de rommene som alle eksisterer, så legge til nye rom.
+     * Denne metoden har støtte for å få inn en liste med flere nye rom.
+     * @param liste ObservableList med nye rom som skal bli lagt til og oppdatert i TableViewen.
+     */
     public void oppdaterTableView(ObservableList<RomTabell> liste) {
         ObservableList<RomTabell> tableViewItems = romTableView.getItems();
         ObservableList<RomTabell> romArrayList = FXCollections.observableArrayList();
@@ -46,11 +43,13 @@ public class RomTableView {
         for (int i = 0; i < liste.size(); i++){
             romArrayList.add(liste.get(i));
         }
-
         romTableView.setItems(romArrayList);
     }
 
-
+    /**
+     * Metode for å returnere tablieView'en.
+     * @return Returnerer tableView'en.
+     */
     public TableView<RomTabell> getRomTableView() {
         return tableView();
     }
