03495c9de9bff4d5b067735e870ba20dae2d75de
dyb0 <dybevik2@gmail.com>
2021-05-03 16:28:33 +0200

    meldingssystem

--- src/com/eksamen/systems/MessageSystem.java
+++ src/com/eksamen/systems/MessageSystem.java
@@ -1,4 +1,36 @@
 package com.eksamen.systems;
 
+import java.time.LocalDateTime;
+
 public class MessageSystem {
+    private String timestamp;
+    private String brukernavn;
+    private String melding;
+
+    //MessageSystem msg = new MessageSystem("Brukernavn", "Melding");
+    public MessageSystem(String brukernavn, String melding){
+        this.brukernavn = brukernavn;
+        this.melding = melding;
+        this.timestamp = hentTidspunkt(LocalDateTime.now());
+    }
+
+    /**
+     * Henter kun tidspunktet i datoen
+     * @param dato
+     * @return Tidspunkt i string
+     */
+    public String hentTidspunkt(LocalDateTime dato){
+        String time = String.valueOf(dato.getHour());
+        String minutt = String.valueOf(dato.getMinute());
+        String sekund = String.valueOf(dato.getSecond());
+        return time+":"+minutt+":"+sekund;
+    }
+
+    public String getBrukernavn() {
+        return brukernavn;
+    }
+
+    public String getMelding() {
+        return melding;
+    }
 }
--- src/com/eksamen/uis/ClientUi.java
+++ src/com/eksamen/uis/ClientUi.java
@@ -1,18 +1,11 @@
 package com.eksamen.uis;
 
 import com.eksamen.uis.layouts.HovedLayout;
-import com.eksamen.uis.layouts.RomChat;
-import com.eksamen.uis.layouts.HovedLayout;
-import com.eksamen.uis.layouts.StartLayout;
-import com.eksamen.uis.layouts.TestLayout;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 
 public class ClientUi {
     private GridPane hovedPane;
     private HovedLayout hovedLayout;
-
     public ClientUi() {
         hovedLayout = new HovedLayout();
         hovedPane = new GridPane();
