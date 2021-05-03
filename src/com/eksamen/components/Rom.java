5a260c9bac4525114035ad8d675cfd19bff124df
233511 <sigve888@gmail.com>
2021-05-03 11:31:19 +0200

    Rom constructor

--- src/com/eksamen/components/Rom.java
+++ src/com/eksamen/components/Rom.java
@@ -1,4 +1,13 @@
 package com.eksamen.components;
 
 public class Rom {
+    private int id;
+    private String romNavn;
+    private String opprettetNavn;
+
+    public Rom(int id, String romNavn, String opprettetNavn) {
+        this.id = id;
+        this.romNavn = romNavn;
+        this.opprettetNavn = opprettetNavn;
+    }
 }
