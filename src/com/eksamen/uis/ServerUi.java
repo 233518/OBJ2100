930485a9714acb5bfe17b6a875555a19478c5266
233518 <233518@usn.no>
2021-05-03 13:55:12 +0200

    Testing

--- src/com/eksamen/uis/ServerUi.java
+++ src/com/eksamen/uis/ServerUi.java
@@ -1,23 +1,20 @@
 package com.eksamen.uis;
 
+import com.eksamen.uis.layouts.HovedLayout;
 import com.eksamen.uis.layouts.TestLayout;
 import javafx.scene.layout.GridPane;
 
 public class ServerUi {
     private GridPane hovedPane;
-    private TestLayout test;
+    private HovedLayout hovedLayout;
 
     public ServerUi() {
-        test = new TestLayout();
         hovedPane = new GridPane();
-        hovedPane.getChildren().add(test.getPane());
+        hovedLayout = new HovedLayout();
+        hovedPane.getChildren().add(hovedLayout.getTabPane());
     }
 
     public GridPane getHovedPane() {
         return hovedPane;
-    }
-
-    public TestLayout getTest() {
-        return test;
     }
 }
