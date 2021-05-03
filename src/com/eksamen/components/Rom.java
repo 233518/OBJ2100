9dc1a5b7d64e72572dc722380477f6f6d8889c3e
233511 <sigve888@gmail.com>
2021-05-03 11:47:31 +0200

    endret på rom

--- src/com/eksamen/components/Rom.java
+++ src/com/eksamen/components/Rom.java
@@ -3,11 +3,11 @@ package com.eksamen.components;
 public class Rom {
     private int id;
     private String romNavn;
-    private String bruker;
+    private Bruker bruker;
 
-    public Rom(int id, String romNavn, String opprettetNavn) {
+    public Rom(int id, String romNavn, Bruker bruker) {
         this.id = id;
         this.romNavn = romNavn; //Navn på rommet
-        this.bruker = opprettetNavn; //Navnet på den som opprettet rommet
+        this.bruker = bruker; //Navnet på den som opprettet rommet
     }
 }
