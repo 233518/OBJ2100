d19a3439c551930fd71e8259d4a9ccdb4c73a49c
233518 <233518@usn.no>
2021-05-04 13:38:45 +0200

    Knapp for send rom

--- src/com/eksamen/systems/ServerInput.java
+++ src/com/eksamen/systems/ServerInput.java
@@ -1,7 +1,11 @@
 package com.eksamen.systems;
 
 import com.eksamen.components.Bruker;
+import com.eksamen.components.Rom;
 import com.eksamen.networking.ServerNetworking;
+import com.eksamen.systems.chatsystem.DeltakerTabell;
+import com.eksamen.systems.chatsystem.InndataTabell;
+import com.eksamen.systems.romsystem.RomSystem;
 import com.eksamen.uis.layouts.HovedLayout;
 import com.eksamen.uis.layouts.RomChat;
 import com.eksamen.uis.layouts.RomListeUI;
@@ -16,12 +20,30 @@ public class ServerInput extends InputSystem {
 
     @Override
     public void sendMelding() {
-
+        romChat.getSendKnapp().setOnAction(actionEvent -> {
+            String melding = romChat.getMeldingsBoks().getText();
+            inndataTabell = new InndataTabell(bruker.getName(), melding);
+            System.out.println(inndataTabell);
+            message.nyMelding(rom, inndataTabell);
+            romChat.oppdaterMeldingListe(message.getMeldinger(rom));
+        });
     }
 
     @Override
     public void opprettRom() {
-
+        romListeUI.getButtonLeggTilRom().setOnAction(actionEvent -> {
+            romSystem = new RomSystem(romListeUI, bruker);
+            Rom rom = new Rom(romListeUI.getTextField().getText(), bruker.getName());
+            romSystem.opprettRom(rom);
+            hovedLayout.lagNyTab(romListeUI.getTextField().getText());
+            deltakerTabell = new DeltakerTabell(bruker.getName());
+            rom.leggTilDeltaker(deltakerTabell);
+            romChat.oppdaterDeltakerListe(romSystem.getDeltakere(rom));
+            romListeUI.skjulOpprettRom();
+            bruker.setRom(rom);
+            setRom();
+            serverNetworking.sendNewRoom(rom.getRomNavn(), bruker.getName());
+        });
     }
 
     @Override
