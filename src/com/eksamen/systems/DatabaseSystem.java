279ca6ec84c8ce4e4b7880a4a6d0012faab65dd6
233511 <sigve888@gmail.com>
2021-05-04 18:32:40 +0200

    Start på DatabaseSystem.java og Logging.java

--- src/com/eksamen/systems/DatabaseSystem.java
+++ src/com/eksamen/systems/DatabaseSystem.java
@@ -5,41 +5,149 @@ import com.eksamen.utils.Logging;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseSystem implements Logging {
-    private Connection con;
+    private Connection sqlConnection;
 
     public DatabaseSystem() {
+        lagOppkoblingTabell();
+        lagOpprettetRomTabell();
+        lagMeldingTabell();
+        lagBliMedTabell();
+        lagForlatTabell();
+    }
+
+    @Override
+    public void lagOppkoblingTabell() {
         try {
-            DriverManager.getConnection("jdbc:sqlite:demo.db");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+            sqlConnection = DriverManager.getConnection("jdbc:sqlite:ChatProgramDB.db");
+            Statement statement = sqlConnection.createStatement();
+            statement.executeUpdate("create table if not exists oppkobling (id integer NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, bruker TEXT, ip TEXT, dato DATETIME)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                sqlConnection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
         }
+    }
+
+    @Override
+    public void lagBliMedTabell() {
+        try {
+            sqlConnection = DriverManager.getConnection("jdbc:sqlite:ChatProgramDB.db");
+            Statement statement = sqlConnection.createStatement();
+            statement.executeUpdate("create table if not exists bliMedRom (id integer NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, bruker TEXT, ip TEXT, rom TEXT, dato DATETIME)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                sqlConnection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void lagForlatTabell() {
+        try {
+            sqlConnection = DriverManager.getConnection("jdbc:sqlite:ChatProgramDB.db");
+            Statement statement = sqlConnection.createStatement();
+            statement.executeUpdate("create table if not exists forlatRom (id integer NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, bruker string, ip string, rom string, dato DATETIME)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                sqlConnection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void lagMeldingTabell() {
+        try {
+            sqlConnection = DriverManager.getConnection("jdbc:sqlite:ChatProgramDB.db");
+            Statement statement = sqlConnection.createStatement();
+            statement.executeUpdate("create table if not exists meldinger (id integer NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, bruker string, melding string, ip string, rom string, dato DATETIME)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                sqlConnection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void lagOpprettetRomTabell() {
+        try {
+            sqlConnection = DriverManager.getConnection("jdbc:sqlite:ChatProgramDB.db");
+            Statement statement = sqlConnection.createStatement();
+            statement.executeUpdate("create table if not exists opprettRom (id integer NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, bruker string, ip string, rom string, dato DATETIME)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                sqlConnection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void loggMelding(String bruker, String melding, String ip, String rom, String dato) {
+        try {
+            sqlConnection = DriverManager.getConnection("jdbc:sqlite:ChatProgramDB.db");
+            Statement statement = sqlConnection.createStatement();
+            //id integer NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY, bruker string, melding string, ip string, rom string, dato DATETIME
+            statement.executeUpdate("insert into meldinger(bruker, melding, ip, rom, dato) values("+bruker+",)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                sqlConnection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void loggLoggInn(String bruker, String melding, String ip, String rom, String dato) {
+
+    }
+
+   // @Override
+   // public void loggLoggInn() {
+//
+   // }
+
+    @Override
+    public void loggBliMed() {
 
     }
 
     @Override
-    public void logMessage() {
+    public void loggForlat() {
 
     }
 
     @Override
-    public void logLogin() {
+    public void loggLoggUt() {
 
     }
 
     @Override
-    public void logJoin() {
-
-    }
-
-    @Override
-    public void logLeave() {
-
-    }
-
-    @Override
-    public void logLogout() {
+    public void logNewRoom() {
 
     }
 }
--- src/com/eksamen/utils/Logging.java
+++ src/com/eksamen/utils/Logging.java
@@ -1,9 +1,15 @@
 package com.eksamen.utils;
 
 public interface Logging {
-    public void logMessage();
-    public void logLogin();
-    public void logJoin();
-    public void logLeave();
-    public void logLogout();
+    public void lagOppkoblingTabell();
+    public void lagBliMedTabell();
+    public void lagForlatTabell();
+    public void lagMeldingTabell();
+    public void lagOpprettetRomTabell();
+    public void loggMelding(String bruker, String melding, String ip, String rom, String dato);
+    public void loggLoggInn(String bruker, String melding, String ip, String rom, String dato);
+    public void loggBliMed();
+    public void loggForlat();
+    public void loggLoggUt();
+    public void logNewRoom();
 }
