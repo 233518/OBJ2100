a614ea5a98ef5c85324fa8f294109f7e9fbf9d02
233511 <sigve888@gmail.com>
2021-05-05 13:49:14 +0200

    JavaDoc LoggLayout.java

--- src/com/eksamen/uis/layouts/LoggLayout.java
+++ src/com/eksamen/uis/layouts/LoggLayout.java
@@ -4,15 +4,26 @@ import com.eksamen.systems.loggsystem.LoggSystem;
 import com.eksamen.systems.loggsystem.LoggTableView;
 import javafx.scene.layout.HBox;
 
+/**
+ * Klasse for LoggLayout
+ */
 public class LoggLayout {
     private HBox hbox;
     //private final LoggTableView loggTableView = new LoggTableView();
 
+    /**
+     * Constructor for å lage nytt UI/Layout til loggsystemet.
+     * @param loggSystem
+     */
     public LoggLayout(LoggSystem loggSystem) {
         this.hbox = new HBox();
         hbox.getChildren().add(loggSystem.getLoggTableView().getLoggTableView());
     }
 
+    /**
+     * Metode for å få tak i hboxen som inneholder LoggLayout/UI
+     * @return Hbox med tableview til Logging.
+     */
     public HBox getHbox() {
         return hbox;
     }
